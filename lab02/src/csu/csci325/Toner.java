60b22c23d875f504fff6b67df7b2c9ac04700f0e
Cameron Cross <Cameron Cross@10.34.2.9>
2018-09-20 21:51:08 -0400

    final attempt i hope....

--- lab02/src/csu/csci325/Toner.java
+++ lab02/src/csu/csci325/Toner.java
@@ -43,7 +43,7 @@ public class Toner
     {
         boolean success;
         
-        if (amt >= 0)
+        if (amt > 0)
         {
             amt -= .05;
 //            if (amt == .04999999999987867)
