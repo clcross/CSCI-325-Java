cebbf1ae1bcbb93d696ab88b1f889301b24a29fe
Cameron Cross <Cameron Cross@10.34.2.9>
2018-10-17 23:07:55 -0400

    test prep code finished

---
+++ TestPrep/src/csu/csci325/DogDriver.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package csu.csci325;
+
+/**
+ *
+ * @author ccross190196
+ * @author aramsey189051
+ */
+public class DogDriver
+{
+    public static void main(String [] args)
+    {
+        DogBreed myBreed = new DogBreed("Green", "curly");
+        WorkingDog myWorkingDog = new WorkingDog("blue", "fluffy", "K9 doggo");
+        NonWorkingDog myNonWorkingDog = new NonWorkingDog("", "", 2);
+        
+        /*
+        * question 12: the lines of code below show polymorphism because
+        * variable polyBreed is a DogBreed variable that can reference objects
+        * from type WorkingDog.
+        */ 
+        DogBreed polyBreed = new WorkingDog("gray", "bigOlTail", "");
+        //polyBreed.setHairColor("gray");
+        System.out.println(polyBreed.getHairColor());
+        System.out.println(polyBreed.getTailStyle());
+        /*
+        * affter running it shows proof of polymorphism because
+        * polyBreed.getHairColor() returns the tailStyle like the override
+        * method in WorkingDog.
+        */
+        
+        // test case a-b
+        myBreed.setHairColor("Brown");
+        System.out.println(myBreed.getHairColor());
+        
+        //test case c-d
+        myBreed.setTailStyle("curled");
+        System.out.println(myBreed.getTailStyle());
+        
+        // test case e
+        myWorkingDog.setJob("K9 Officer");
+        System.out.println(myWorkingDog.getJob());
+        
+        // test case f-h
+        myNonWorkingDog.setSize(0);
+        System.out.println(myNonWorkingDog.getSize());
+        
+    }
+}
---
+++ TestPrep/src/csu/csci325/NonWorkingDog.java
@@ -0,0 +1,69 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package csu.csci325;
+
+/**
+ *
+ * @author ccross190196
+ * @author aramsey189051
+ */
+public class NonWorkingDog extends DogBreed
+{
+    private int mSize;
+    private String mHairColor;
+    private String mTailStyle;
+    public NonWorkingDog(String h, String t, int s)
+    {
+        super(h, t);
+        
+        mSize = s;
+        mHairColor = h;
+        mTailStyle = t;
+    }
+    
+    public void setSize(int s)
+    {
+        if (s <= 3)
+        {
+           if (s >= 1)
+           {
+              mSize = s; 
+           }
+           else
+           {
+               System.out.println("The value entered was less than 1");
+               mSize = 0;
+           }
+        }
+        else if (s > 3)
+        {
+            System.out.println("The value entered was greater than 3");
+            mSize = 0;
+        }
+    }
+    
+    public String getSize()
+    {
+        String sizeInWords;
+        switch (mSize)
+        {
+            case 1:
+                sizeInWords = "small";
+                break;
+            case 2:
+                sizeInWords = "medium";
+                break;
+            case 3:
+                sizeInWords = "large";
+                break;
+            default:
+                sizeInWords = "invalid entry";
+                break;
+        }
+        
+        return sizeInWords;
+    }
+}
