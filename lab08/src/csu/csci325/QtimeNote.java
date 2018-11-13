1076e44daf867fde1c5d2c73d86f13ac3c103a61
Cameron Cross <Cameron Cross@10.34.2.9>
2018-11-13 16:20:38 -0500

    Qtime check

---
+++ lab08/src/csu/csci325/QtimeNote.java
@@ -0,0 +1,69 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package csu.csci325;
+
+import java.util.Date;
+
+/**
+ *
+ * @author Cameron Cross
+ */
+public class QtimeNote implements Comparable<QtimeNote>
+{
+    Date date;
+    String bibBook;
+    String bibVerse;
+    String bibNote;
+    public QtimeNote(String book, String verse, String note)
+    {
+        bibBook = book;
+        bibVerse = verse;
+        bibNote = note;
+    }
+    
+    @Override
+    public int compareTo(QtimeNote n)
+    {
+        int retVal;
+        
+        if (bibBook.concat(bibVerse).equals(n.bibBook.concat(n.bibVerse)))
+        {
+            retVal = 0;
+        }
+        else
+        {
+            retVal = -1;
+        }
+        
+        return retVal;
+    }
+    
+    public Date getDate()
+    {
+        date = new Date();
+       return date; 
+    }
+    
+    public String getBookOfBible()
+    {
+        return bibBook;
+    }
+    
+    public String getVerseRef()
+    {
+        return bibVerse;
+    }
+    
+    public String getUserNotes()
+    {
+        return bibNote;
+    }
+    
+    public void updateUserNotes(String userNotes)
+    {
+        bibNote = userNotes;
+    }
+}
