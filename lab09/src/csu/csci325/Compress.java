4b69bb169165434458b384f3b8d21ae9bfaedfea
Cameron Cross <Cameron Cross@10.34.2.9>
2018-11-30 00:23:28 -0500

    final

--- lab09/src/csu/csci325/Compress.java
+++ lab09/src/csu/csci325/Compress.java
@@ -13,64 +13,70 @@ import java.util.Stack;
  * @author Cameron Cross
  */
 public class Compress {
-    private static Stack<String> stack;
+    //private static Stack<String> stack;
     public Compress(){
-        stack = new Stack<>();
+        //stack = new Stack<>();
     }
     
     public static String compress(String s){
+        String comp = null;
         
-        String finalCompressedString = "";
-        String lastElement="";
-        char[] charArray = s.toCharArray();
-        int elementCount = 0;
-        
-        if (s == null || s.length() == 0){
-            return null;
-        }
-            
-        for (int i = 0; i < charArray.length; i++){
-            char currentElement = charArray[i];
-            if (i == 0){
-                stack.push((currentElement+""));
-                continue;
-            } 
-            else{
-                if ((currentElement+"").equalsIgnoreCase((String)stack.peek())){
-                    stack.push(currentElement + "");
-                    
-                    if(i==charArray.length-1)
-                    {
-                        while (!stack.isEmpty()){
-
-                            lastElement = (String)stack.pop();
-                            elementCount++;
-                        }
-
-                        finalCompressedString += lastElement + "" + elementCount;
-                    }
-                    else{
-                       continue; 
-                    }
-                }
-                else{
-                    while (!stack.isEmpty()){
-
-                        lastElement = (String)stack.pop();
-                        elementCount++;
-                    }
-                    
-                    finalCompressedString += lastElement + "" + elementCount;
-                    elementCount=0;
-                    stack.push(currentElement+"");
-                }
-            }
-        }
-        if (finalCompressedString.length() >= s.length()){
-            return s;
-        }
-        else{
-            return finalCompressedString;
-        }
+        return comp;
     }
+    
+//    public static String compress(String s){
+//        
+//        String finalCompressedString = "";
+//        String lastElement="";
+//        char[] charArray = s.toCharArray();
+//        int elementCount = 0;
+//        
+//        if (s == null || s.length() == 0){
+//            return null;
+//        }
+//            
+//        for (int i = 0; i < charArray.length; i++){
+//            char currentElement = charArray[i];
+//            if (i == 0){
+//                stack.push((currentElement+""));
+//                continue;
+//            } 
+//            else{
+//                if ((currentElement+"").equalsIgnoreCase((String)stack.peek())){
+//                    stack.push(currentElement + "");
+//                    
+//                    if(i==charArray.length-1)
+//                    {
+//                        while (!stack.isEmpty()){
+//
+//                            lastElement = (String)stack.pop();
+//                            elementCount++;
+//                        }
+//
+//                        finalCompressedString += lastElement + "" + elementCount;
+//                    }
+//                    else{
+//                       continue; 
+//                    }
+//                }
+//                else{
+//                    while (!stack.isEmpty()){
+//
+//                        lastElement = (String)stack.pop();
+//                        elementCount++;
+//                    }
+//                    
+//                    finalCompressedString += lastElement + "" + elementCount;
+//                    elementCount=0;
+//                    stack.push(currentElement+"");
+//                }
+//            }
+//        }
+//        if (finalCompressedString.length() >= s.length()){
+//            return s;
+//        }
+//        else{
+//            return finalCompressedString;
+//        }
+//    }
 }
