41895e0b988d3e6f65712b4abf24f56bc10a0618
Cameron Cross <Cameron Cross@10.34.2.9>
2018-11-29 23:02:27 -0500

    Pal

--- lab09/src/csu/csci325/Palindrome.java
+++ lab09/src/csu/csci325/Palindrome.java
@@ -11,4 +11,34 @@ package csu.csci325;
  */
 public class Palindrome {
     
+    public Palindrome(){
+        
+    }
+    
+    public static boolean checkPal(String s){
+        boolean retVal;
+        String str;
+        String temp = s.trim();
+        temp = s.toUpperCase();
+        str = temp.replaceAll("[A-Z]", "");
+        retVal = isIt(str);
+        return retVal;
+    }
+    
+    public static boolean isIt(String s){
+        int last = s.length() - 1;
+        String str;
+        boolean retVal = true;
+        if(s.length() <= 1 && s.length() >= 0){
+            retVal = true;
+        }
+        else if(s.charAt(0) == s.charAt(last)){
+            str = s.substring(1, last);
+            retVal = isIt(str);
+        }
+        else{
+            retVal = false;
+        }
+        return retVal;
+    }
 }
